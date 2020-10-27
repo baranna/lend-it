c77936a1fd5c7d626279b8cd82d31cc1bf3cdbb5
Baross Anna <baross.anna@gmail.com>
2020-10-27 23:22:30 +0100

    add css framework, basic components

--- lend-it-frontend/src/app/app-routing.module.ts
+++ lend-it-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'devices',
+		loadChildren: () => import('./device/device.module').then(m => m.DeviceModule),
+	},
+	{
+		path: '',
+		redirectTo: 'devices',
+		pathMatch: 'full',
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ lend-it-frontend/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+	declarations: [ NavbarComponent ],
+	exports: [
+		NavbarComponent,
+	],
+	imports: [
+		CommonModule,
+	],
+})
+export class CoreModule {
+}
